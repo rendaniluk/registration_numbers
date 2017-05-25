aef0f16f9c5ebe10ddd8995b6c3e2f0d1e502134
rendaniluk <lukhwa11582924>
2017-05-25 15:04:22 +0200

    car  regnum

--- registration_numbers.js
+++ registration_numbers.js
@@ -4,7 +4,7 @@ var Btn = document.querySelector('.tx')
 //add eventlistener to add button
 Btn.addEventListener('click', () => {
     if (addRegNum.value.length > 0) { //to make sure nothing is added to list when there is nothing on textbox.
-        var ul = document.getElementsByTagName('ul')[0];
+        var ul = document.getElementsByTagName('ul')[1];
         var li = document.createElement('li');
         li.textContent = addRegNum.value.substr(0, 2).toUpperCase() + ' ' + addRegNum.value.substr(2).toUpperCase();
         ul.appendChild(li);
